30802bf1e9352b6e8c262fbfdb0be5b89c25d96b
Manal Al-Subaei <alsubaeimanal@gmail.com>
2021-09-09 17:04:14 +0300

    recode the js to jQuery, but still some logical errors

--- js/style.js
+++ js/style.js
@@ -1,48 +1,49 @@
 //******************************************************************************* */
 // global variables 
 
-const signup_form = document.forms["signup_form"];
-const login_form = document.forms["login_form"];
+const signup_form = $("form#signup_form");
+const login_form = $("form#login_form");
 
-const signup = document.getElementsByClassName("signup_btn");
-const login = document.getElementsByClassName("login_btn");
+const signup = $(".signup_btn");
+const login = $(".login_btn");
 
 //input of password of login form
-const password = document.getElementById("pwd");
+const password = $("#pwd");
 
-const FurnitureShipping_form = document.getElementById("FurnitureShipping_form");
-const CarShipping_form = document.getElementById("CarShipping_form");
-const GoodsShipping_form = document.getElementById("GoodsShipping_form");
+const FurnitureShipping_form = $("#FurnitureShipping_form");
+const CarShipping_form = $("#CarShipping_form");
+const GoodsShipping_form = $("#GoodsShipping_form");
 
-const focus = document.getElementsByClassName("form-check-inline");
+const focus = $(".form-check-inline");
 
-const Furniture = document.getElementsByClassName("Furniture");
-const Car = document.getElementsByClassName("Car");
-const Goods = document.getElementsByClassName("Goods");
+const Furniture = $(".Furniture");
+const Car = $(".Car");
+const Goods = $(".Goods");
 
 let g;
 
-const Header_Links = document.getElementsByClassName("nav-active");
+const Header_Links = $(".nav-active");
 
 //******************************************************************************* */
 // first actions
+$(document).ready(function() {
+    $("#forgot_password").css("visibility", "hidden");
+    $("#enter_password").css("visibility", "hidden");
+    signup_form.css("display", "none");
+    FurnitureShipping_form.hide();
+    CarShipping_form.hide();
+    GoodsShipping_form.hide();
+    login_form.css("display", "block");
+});
 
-if (document.readyState === "loading") {
-    document.getElementById("forgot_password").style.visibility = "hidden";
-    document.getElementById("enter_password").style.visibility = "hidden";
-    signup_form.style.display = "none";
-    FurnitureShipping_form.style.display = "none";
-    CarShipping_form.style.display = "none";
-    GoodsShipping_form.style.display = "none";
-    login_form.style.display = "block";
-}
 
 // //******************************************************************************* */.
 // //focus and blur background color of radio button
 
 const bg_radio_button = function(x) {
-    const buttons = x.querySelectorAll('input[type="radio"]');
-    const bg_button = x.getElementsByClassName("form-check-inline");
+    const buttons = document.querySelectorAll(x.val() + 'input[type="radio"]');
+    const bg_button = $("." + x.val() + "form-check-inline");
+    console.log(bg_button);
     for (let i = 0; i < buttons.length; i++) {
         if (buttons[i].checked) {
             bg_button[i].classList.remove("focus_color");
@@ -63,20 +64,10 @@ const bg_radio_button = function(x) {
 //made by Eng_yaserhadi
 
 for (let i = 0; i < Header_Links.length; i++) {
-    Header_Links[i].addEventListener("click", function() {
-        removeClass(Header_Links, "nav-active-style");
-        addClass(this, "nav-active-style");
+    Header_Links[i].click(function() {
+        Header_Links.removeClass("nav-active-style");
+        Header_Links.addClass("nav-active-style");
     });
-}
-
-function removeClass(elemnts, removedCalssName) {
-    for (let i = 0; i < elemnts.length; i++) {
-        elemnts[i].classList.remove(removedCalssName);
-    }
-}
-
-function addClass(currentElement, addedClassname) {
-    currentElement.classList.add(addedClassname);
 }
 
 
@@ -84,18 +75,18 @@ function addClass(currentElement, addedClassname) {
 //display FurnitureShipping_form
 
 const display_FurnitureShipping_form = function() {
-    CarShipping_form.style.display = "none";
-    GoodsShipping_form.style.display = "none";
-    FurnitureShipping_form.getElementsByClassName("Furniture")[0].checked = "true";
+    CarShipping_form.hide();
+    GoodsShipping_form.hide();
+    $("#FurnitureShipping_form .Furniture")[0].checked = "true";
     if (g != 1)
-        FurnitureShipping_form.getElementsByClassName("Furniture")[1].checked = "true";
+        $("#FurnitureShipping_form .Furniture")[1].checked = "true";
     if (g == 1)
         bg_radio_button(FurnitureShipping_form);
-    FurnitureShipping_form.style.display = "block";
+    FurnitureShipping_form.show();
 };
 
 for (let i = 0; i < Furniture.length; i++) {
-    Furniture[i].addEventListener("click", display_FurnitureShipping_form, true);
+    Furniture[i].addEventListener("click", display_FurnitureShipping_form);
 }
 
 
@@ -104,8 +95,8 @@ for (let i = 0; i < Furniture.length; i++) {
 
 function background_pink() {
 
-    if (!document.getElementById("email").value) {
-        document.getElementById("email").style.background = "pink";
+    if (!$("#email").value) {
+        $("#email").css("background-color", "pink");
     }
 
 
@@ -115,46 +106,46 @@ function background_pink() {
     }
 }
 
-document.getElementById("submit_login").addEventListener("click", background_pink);
-document.getElementById("submit_signup").addEventListener("click", background_pink);
+$("#submit_login").click(background_pink);
+$("#submit_signup").click(background_pink);
 
 
 //******************************************************************************* */
 //display "forgot Password?"
 
 const forgot_password = function() {
-    document.getElementById("email").style.background = "white";
-    document.getElementById("forgot_password").style.visibility = "visible"; //hidden
+    $("#email").css("background-color", "white");
+    $("#forgot_password").css("visibility", "visible"); //hidden
 };
 
-password.addEventListener("focus", forgot_password);
+password.focus(forgot_password);
 
 
 //******************************************************************************* */
 //display "enter the password"
 
 const enter_password = function() {
-    if (!password.value && document.getElementById("email").value) {
+    if (!password.value && $("#email").value) {
         password.required;
-        document.getElementById("enter_password").style.visibility = "visible"; //hidden
-        password.style.background = "pink";
+        $("#enter_password").css("visibility", "visible"); //hidden
+        password.css("background-color", "pink");
         return false;
     }
     return true;
 };
 
-document.getElementById("submit_login").addEventListener("click", enter_password);
+$("#submit_login").click(enter_password);
 
 
 //******************************************************************************* */
 //display Signup
 
 const display_signup = function() {
-    login_form.style.display = "none";
-    FurnitureShipping_form.style.display = "none";
-    CarShipping_form.style.display = "none";
-    GoodsShipping_form.style.display = "none"
-    signup_form.style.display = "block";
+    login_form.css("display", "none");
+    FurnitureShipping_form.hide();
+    CarShipping_form.hide();
+    GoodsShipping_form.hide();
+    signup_form.css("display", "block");
 };
 
 for (let i = 0; i < signup.length; i++)
@@ -165,11 +156,11 @@ for (let i = 0; i < signup.length; i++)
 //display Login
 
 const display_login = function() {
-    signup_form.style.display = "none";
-    FurnitureShipping_form.style.display = "none";
-    CarShipping_form.style.display = "none";
-    GoodsShipping_form.style.display = "none"
-    login_form.style.display = "block";
+    signup_form.css("display", "none");
+    FurnitureShipping_form.hide();
+    CarShipping_form.hide();
+    GoodsShipping_form.hide();
+    login_form.css("display", "block");
 };
 
 for (let i = 0; i < login.length; i++)
@@ -180,7 +171,7 @@ for (let i = 0; i < login.length; i++)
 //disdisplay "color of empty fields of sign up"
 
 function background_White(y) {
-    document.getElementById(y).style.background = "white";
+    $("#" + y).css("background-color", "white");
 }
 
 for (let i = 0; i < signup_form.length - 1; i++) {
@@ -197,20 +188,20 @@ for (let i = 0; i < signup_form.length - 1; i++) {
 const display_FurnitureShipping = function(x) {
     let flag = 0;
     for (let i = 0; i < x.length - 1; i++) {
-        if (!document.getElementById(x.elements[i].id).value) flag++;
+        if (!$("#" + x.elements[i].id).value) flag++;
     }
     if (flag == 0) {
-        login_form.style.display = "none";
-        signup_form.style.display = "none";
+        login_form.css("display", "none");
+        signup_form.css("display", "none");
         for (let i = 0; i < Furniture.length; i++) {
             Furniture[i].checked = "true";
         }
         bg_radio_button(FurnitureShipping_form);
-        FurnitureShipping_form.style.display = "block";
+        FurnitureShipping_form.show();
     }
 };
 
-document.getElementById("submit_login").addEventListener("click", function() {
+$("#submit_login").click(function() {
     if (!enter_password())
         background_pink();
     else
@@ -218,16 +209,16 @@ document.getElementById("submit_login").addEventListener("click", function() {
 });
 
 /**Yh 20082021 start:try onsubmit */
-document.getElementById("submit_login").addEventListener("submit", function(event) {
+$("#submit_login").submit(function(event) {
     event.preventDefault();
 });
 /**Yh 20082021 End:try onsubmit */
 
-document.getElementById("submit_signup").addEventListener("click", function() {
+$("#submit_signup").click(function() {
     display_FurnitureShipping(signup_form);
 });
 
-document.getElementById("submit_signup").addEventListener("submit", function(event) {
+$("#submit_signup").submit(function(event) {
     event.preventDefault();
 });
 
@@ -236,87 +227,73 @@ document.getElementById("submit_signup").addEventListener("submit", function(eve
 //display CarShipping_form
 
 const display_CarShipping_form = function() {
-    FurnitureShipping_form.style.display = "none";
-    GoodsShipping_form.style.display = "none";
-    CarShipping_form.getElementsByClassName("Car")[0].checked = "true";
-    CarShipping_form.getElementsByClassName("Car")[1].checked = "true";
+    FurnitureShipping_form.hide();
+    GoodsShipping_form.hide();
+    $("#CarShipping_form .Car")[0].checked = "true";
+    $("#CarShipping_form .Car")[1].checked = "true";
     bg_radio_button(CarShipping_form);
-    CarShipping_form.style.display = "block";
+    CarShipping_form.show();
 };
 
 for (let i = 0; i < Car.length; i++) {
-    Car[i].addEventListener("click", display_CarShipping_form, true);
+    Car[i].addEventListener("click", display_CarShipping_form);
 }
 
 //******************************************************************************* */
 //display GoodsShipping_form
 
 const display_GoodsShipping_form = function() {
-    CarShipping_form.style.display = "none";
-    FurnitureShipping_form.style.display = "none";
-    GoodsShipping_form.getElementsByClassName("Goods")[0].checked = "true";
-    GoodsShipping_form.getElementsByClassName("Goods")[1].checked = "true";
+    CarShipping_form.hide();
+    FurnitureShipping_form.hide();
+    $("#GoodsShipping_form .Goods")[0].checked = "true";
+    $("#GoodsShipping_form .Goods")[1].checked = "true";
     bg_radio_button(GoodsShipping_form);
-    GoodsShipping_form.style.display = "block";
+    GoodsShipping_form.show();
 };
 
 for (let i = 0; i < Goods.length; i++) {
     if (i !== 1) {
-        Goods[i].addEventListener("click", display_GoodsShipping_form, true);
+        Goods[i].addEventListener("click", display_GoodsShipping_form);
     } else {
         g = 1;
-        Goods[i].addEventListener("click", display_FurnitureShipping_form, true);
+        Goods[i].addEventListener("click", display_FurnitureShipping_form);
     }
 }
 
 
 //******************************************************************************* */
 //functions of plus and minus button
-//yh 04/09/2021
 
-//increase/decrease Bedrooms functions  
-function add_Bedrooms() {
-    var value = parseInt(document.getElementById('Bedrooms_number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('Bedrooms_number').value = value;
-}
+function incrementValue(e) {
+    e.preventDefault();
+    var fieldName = $(e.target).data('field');
+    var parent = $(e.target).closest('div');
+    var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
 
-function sub_Bedrooms() {
-    var value = parseInt(document.getElementById('Bedrooms_number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value < 1 ? value = 1 : '';
-    value--;
-    document.getElementById('Bedrooms_number').value = value;
+    if (!isNaN(currentVal)) {
+        parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
+    } else {
+        parent.find('input[name=' + fieldName + ']').val(0);
+    }
 }
 
-//increase/decrease Bedrooms functions
-function add_Apartment() {
-    var value = parseInt(document.getElementById('Apartment_number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('Apartment_number').value = value;
+function decrementValue(e) {
+    e.preventDefault();
+    var fieldName = $(e.target).data('field');
+    var parent = $(e.target).closest('div');
+    var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+
+    if (!isNaN(currentVal) && currentVal > 0) {
+        parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
+    } else {
+        parent.find('input[name=' + fieldName + ']').val(0);
+    }
 }
 
-function sub_Apartment() {
-    var value = parseInt(document.getElementById('Apartment_number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value < 1 ? value = 1 : '';
-    value--;
-    document.getElementById('Apartment_number').value = value;
-}
-//increase/decrease Bedrooms functions
-function add_Electronic() {
-    var value = parseInt(document.getElementById('Electronic_number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value++;
-    document.getElementById('Electronic_number').value = value;
-}
+$('.input-group').on('click', '.button-plus', function(e) {
+    incrementValue(e);
+});
 
-function sub_Electronic() {
-    var value = parseInt(document.getElementById('Electronic_number').value, 10);
-    value = isNaN(value) ? 0 : value;
-    value < 1 ? value = 1 : '';
-    value--;
-    document.getElementById('Electronic_number').value = value;
-}
+$('.input-group').on('click', '.button-minus', function(e) {
+    decrementValue(e);
+});
